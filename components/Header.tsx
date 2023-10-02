8ba45a2c69fbe78658775aba2fe22a13917d8bba
ayushsnha <ayush_snha@rediffmail.com>
2023-10-02 17:57:01 +0530

    js-mastery-seo

---
+++ components/Header.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+interface Props {
+  title: string;
+  query: string;
+  category: string
+}
+
+const Header = ({title, query, category}:Props) => {
+
+  if (query && category) {
+    return (
+      <h1 className='heading3 self-start text-white-800'>
+        Search Results for &quot;{query}&quot; in <span className='capitalize'>{category}</span>
+      </h1>
+    )
+  }
+  
+  if(category) {
+    return (
+      <h1 className='heading3 self-start text-white-800'>
+        <span className='capitalize'>{category}</span>
+      </h1>
+    )
+  }
+
+  if(query) {
+    return (
+      <h1 className='heading3 self-start text-white-800'>
+        Search Results for &quot;{query}&quot;
+      </h1>
+    )
+  }
+
+
+  return (
+    <h1 className='heading3 self-start text-white-800'>No Results</h1>
+  )
+}
+
+export default Header
